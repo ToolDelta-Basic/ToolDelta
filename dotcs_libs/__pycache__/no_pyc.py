10fb7e03cf79458a634afa0bfc908d47ed716d66
SuperScriptHigh <112140997+SuperScript-PRC@users.noreply.github.com>
2023-04-30 12:51:26 +0800

    Delete dotcs_libs directory

--- dotcs_libs/__pycache__/no_pyc.py
+++
@@ -1,6 +0,0 @@
-import os, shutil
-for file in os.listdir():
-    print(file)
-    if file.endswith(".pyc"):
-        shutil.copy(file, file.replace(".cpython-310", ""))
-        
--- dotcs_libs/basic_mods.py
+++
@@ -1,4 +0,0 @@
-import time, os, sys, threading, json, traceback, datetime, subprocess, pymysql
-import psutil, requests, ctypes, nbt, qrcode, rich, numpy, bdx_work_shop
-from typing import Callable
-dotcs_module_env = {"time": time, "os": os, "json": json, "threading": threading, "requests": requests, "traceback": traceback, "qrcode": qrcode, "numpy": numpy, "bdx_work_shop": bdx_work_shop, "psutil": psutil, "pymysql": pymysql}
