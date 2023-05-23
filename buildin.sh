836b4e6c222ccf26d0599abe39ab88855a0fd0c4
SuperScript <112140997+SuperScript-PRC@users.noreply.github.com>
2023-05-23 12:11:23 +0800

    Update buildin.sh
    better buildmode for ToolDelta

--- buildin.sh
+++ buildin.sh
@@ -1,5 +1,8 @@
-echo start
+echo Starting to build ToolDelta executable file...
 rm ToolDelta
 pyinstaller -F ToolDelta.py
 chmod u+x dist/ToolDelta
 sudo mv dist/ToolDelta .
+rm -rf build
+rm ToolDelta.spec
+echo Finished!
