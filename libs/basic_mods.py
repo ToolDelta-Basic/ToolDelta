96d76f569af07ab7f2cdfa2e671a71eb177d246b
SuperScript <112140997+SuperScript-PRC@users.noreply.github.com>
2023-07-25 06:33:49 +0800

    Update basic_mods.py
    add a module asyncio and sort thr modules

--- libs/basic_mods.py
+++ libs/basic_mods.py
@@ -1,4 +1,4 @@
-import time, os, sys, threading, json, traceback, datetime, platform, subprocess, pymysql, getpass, orjson, socket, websockets
-import psutil, requests, ctypes, nbt, qrcode, logging, tqdm
+import time, os, sys, threading, json, traceback, datetime, platform, subprocess, socket, logging, ctypes, asyncio
+import psutil, requests, nbt, qrcode, tqdm, getpass, orjson, pymysql, websockets
 from typing import Callable
 dotcs_module_env = {"time": time, "os": os, "json": json, "threading": threading, "requests": requests, "traceback": traceback, "qrcode": qrcode, "psutil": psutil, "pymysql": pymysql, "socket": socket, "websockets": websockets}
