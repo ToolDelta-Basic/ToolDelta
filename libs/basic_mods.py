b824757b6b1413f702608bf56a3281353ec5b37c
xingchen <123609288+xingchentye@users.noreply.github.com>
2023-07-27 20:37:03 +0800

    Fix bug

--- libs/basic_mods.py
+++ libs/basic_mods.py
@@ -1,4 +1,4 @@
 import time, os, sys, threading, json, traceback, datetime, platform, subprocess, socket, logging, ctypes, asyncio
-import psutil, requests, nbt, qrcode, tqdm, getpass, orjson, pymysql, websockets
+import psutil, requests, nbt, qrcode, tqdm, getpass, orjson, pymysql, websockets, ujson
 from typing import Callable
 dotcs_module_env = {"time": time, "os": os, "json": json, "threading": threading, "requests": requests, "traceback": traceback, "qrcode": qrcode, "psutil": psutil, "pymysql": pymysql, "socket": socket, "websockets": websockets}
