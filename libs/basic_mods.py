c16d004ed18edcfb23cc150d6b15068ddd55f126
xingchen <123609288+xingchentye@users.noreply.github.com>
2023-07-30 16:49:34 +0800

    Add files via upload

--- libs/basic_mods.py
+++ libs/basic_mods.py
@@ -1,4 +1,4 @@
 import time, os, sys, threading, json, traceback, datetime, platform, subprocess, socket, logging, ctypes, asyncio
-import psutil, requests, nbt, qrcode, tqdm, getpass, orjson, pymysql, websockets, ujson, hashlib, base64
+import psutil, requests, nbt, qrcode, getpass, orjson, pymysql, websockets, ujson, hashlib, base64, alive_progress
 from typing import Callable
 dotcs_module_env = {"time": time, "os": os, "json": json, "threading": threading, "requests": requests, "traceback": traceback, "qrcode": qrcode, "psutil": psutil, "pymysql": pymysql, "socket": socket, "websockets": websockets}
