4473347574ad8f22f8a2130d2b9e94281d8b0205
SuperScript <112140997+SuperScript-PRC@users.noreply.github.com>
2023-07-30 18:57:04 +0800

    remove 'orjson'

--- libs/basic_mods.py
+++ libs/basic_mods.py
@@ -1,4 +1,4 @@
 import time, os, sys, threading, json, traceback, datetime, platform, subprocess, socket, logging, ctypes, asyncio
-import psutil, requests, nbt, qrcode, getpass, orjson, pymysql, websockets, ujson, hashlib, base64, rich.progress
+import psutil, requests, nbt, qrcode, getpass, pymysql, websockets, ujson, hashlib, base64, rich.progress
 from typing import Callable
 dotcs_module_env = {"time": time, "os": os, "json": json, "threading": threading, "requests": requests, "traceback": traceback, "qrcode": qrcode, "psutil": psutil, "pymysql": pymysql, "socket": socket, "websockets": websockets}
