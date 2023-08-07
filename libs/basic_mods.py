4bfa743ed22918a2cd4ce34c425a1da73f47234e
SuperScript <112140997+SuperScript-PRC@users.noreply.github.com>
2023-08-08 07:20:55 +0800

    neccessary modules added

--- libs/basic_mods.py
+++ libs/basic_mods.py
@@ -1,4 +1,4 @@
-import time, os, sys, threading, json, traceback, datetime, platform, subprocess, socket, logging, ctypes, asyncio
+import time, os, sys, threading, json, traceback, datetime, platform, subprocess, socket, logging, ctypes, asyncio, copy, math, random
 import psutil, requests, nbt, qrcode, getpass, pymysql, websockets, ujson, hashlib, base64, rich.progress, tqdm
 from typing import Callable
 dotcs_module_env = {"time": time, "os": os, "json": json, "threading": threading, "requests": requests, "traceback": traceback, "qrcode": qrcode, "psutil": psutil, "pymysql": pymysql, "socket": socket, "websockets": websockets}
