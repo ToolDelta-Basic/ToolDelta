5b72c86a5bb04b33feedffee892a5969153bc018
SuperScript-PRC <2528622340@qq.com>
2024-02-04 21:34:19 +0800

    new file:   ToolDelta/neo_libs/neomg_android_amd64.h 	new file:   ToolDelta/neo_libs/neomg_android_amd64.so 	new file:   ToolDelta/neo_libs/neomg_android_arm64.h 	new file:   ToolDelta/neo_libs/neomg_android_arm64.so 	new file:   ToolDelta/neo_libs/neomg_linux_aarch64.h 	new file:   ToolDelta/neo_libs/neomg_linux_aarch64.so 	new file:   ToolDelta/neo_libs/neomg_linux_amd64.h 	new file:   ToolDelta/neo_libs/neomg_linux_amd64.so 	new file:   ToolDelta/neo_libs/neomg_linux_arm64.h 	new file:   ToolDelta/neo_libs/neomg_linux_arm64.so 	new file:   ToolDelta/neo_libs/neomg_macos_amd64.dylib 	new file:   ToolDelta/neo_libs/neomg_macos_amd64.h 	new file:   ToolDelta/neo_libs/neomg_macos_arm64.dylib 	new file:   ToolDelta/neo_libs/neomg_macos_arm64.h 	new file:   ToolDelta/neo_libs/neomg_windows_amd64.dll 	new file:   ToolDelta/neo_libs/neomg_windows_amd64.h 	new file:   ToolDelta/neo_libs/neomg_windows_arm64.dll 	new file:   ToolDelta/neo_libs/neomg_windows_arm64.h 	new file:   build_points.sh

---
+++ build_points.sh
@@ -0,0 +1,5 @@
+cd /root/ToolDelta/neo-omega-kernel/neomega/entries/access_point_entry/cli
+./cmp.sh
+cd /root/ToolDelta/neo-omega-kernel/neomega/entries/minimal_end_point_entry/cli
+./cmp.sh 
+cd /root/ToolDelta
