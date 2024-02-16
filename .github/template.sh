35b3900b88d3096b4f05c10aa63b0b79e89c547d
wling <wlingzhenyu@163.com>
2024-02-16 21:41:17 +0800

    🎨 更新模板

--- .github/template.sh
+++ .github/template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 设置安装目录
-install_dir="/opt/tooldelta"
+install_dir="$PWD/tooldelta"
 # 设置应用程序名称
 app_name="ToolDelta"
 # 设置快捷指令
@@ -8,6 +8,13 @@ shortcut_command="td"
 # 设置 GitHub release URL
 github_release_url="https://mirror.ghproxy.com/https://github.com/ToolDelta/ToolDelta/releases/download/{{VERSION}}/ToolDelta-linux"
 
+function EXIT_FAILURE(){
+    exit -1
+}
+
+
+
+function download_exec(){
 # 权限
 mkdir -p "$install_dir"
 chown -R $(whoami):$(whoami) "$install_dir"
@@ -36,5 +43,45 @@ fi
 echo "pushd $install_dir && ./$app_name && popd " >  "$install_dir/start.sh"
 chmod +x "$install_dir/start.sh"
 
-echo "安装完成。您现在可以在命令行中输入 '$shortcut_command' 启动 $app_name。"
 popd
+}
+
+if [[ $(uname) == "Darwin" ]]; then
+    PLANTFORM="Macos_x86_64"
+elif [[ $(uname -o) == "GNU/Linux" ]] || [[ $(uname -o) == "GNU/Linux" ]]; then
+    PLANTFORM="Linux_x86_64"
+    if [[ $(uname -m) != "x86_64" ]]; then
+        echo "不支持非64位的Linux系统"
+        EXIT_FAILURE
+    fi
+elif [[ $(uname -o) == "Android" ]]; then
+    PLANTFORM="Andorid_armv8"
+    if [[ $(uname -m) == "armv7" ]]; then
+        echo "不支持armv7的Andorid系统"
+        EXIT_FAILURE
+    fi
+    echo "检测文件权限中..."
+    if [ ! -x "/sdcard/Download" ]; then
+        echo "请给予 termux 文件权限 ~"
+        sleep 2
+        termux-setup-storage
+    fi
+    if [ -x "/sdcard/Download" ]; then
+        echo -e ""
+        # green_line "太好了，omega将被保存到downloads文件夹下，你可以从任何文件管理器中找到它了"
+        # working_dir="/sdcard/Download"
+        # executable="/sdcard/Download/fastbuilder"
+    else
+        red_line "不行啊，没给权限"
+        EXIT_FAILURE
+    fi
+else
+    echo "不支持该系统，你的系统是"
+    uname -a
+fi
+
+
+
+
+download_exec
+echo "安装完成啦，您现在可以在命令行中输入 '$shortcut_command' 来启动 $app_name。"
