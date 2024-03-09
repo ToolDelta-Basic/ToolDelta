6542cdfcec48584fcc6b95671bae5480e15241b1
xingchentye <xingchenawa@qq.com>
2024-03-09 15:02:15 +0800

    因暂时无法继续正常进行代码编写,当前会导致程序无法正常运行,暂时注释game_texts

--- tooldelta/game_texts.py
+++ tooldelta/game_texts.py
@@ -4,7 +4,8 @@ from .color_print import Print
 class Game_Texts(object):
     def __init__(self) -> None:
         # 检测更新!
-        if not os.path.exists("data/game_texts"):
-            version_match = re.match(r"v(\d+\.\d+\.\d+)", requests.get("https://api.github.com/repos/ToolDelta/ToolDelta-Game_Texts/releases/latest").json()["tag_name"])
-            open("data/game_texts/version","w").write(version_match)
-            packets_url = f"https://github.com/ToolDelta/ToolDelta-Game_Texts/releases/download/{version_match}"
+        # if not os.path.exists("data/game_texts"):
+        #     version_match = re.match(r"v(\d+\.\d+\.\d+)", requests.get("https://api.github.com/repos/ToolDelta/ToolDelta-Game_Texts/releases/latest").json()["tag_name"])
+        #     open("data/game_texts/version","w").write(version_match)
+        #     packets_url = f"https://github.com/ToolDelta/ToolDelta-Game_Texts/releases/download/{version_match}"
+        pass
