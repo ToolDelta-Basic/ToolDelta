6ade0ac778f9790f7f14419f125a4d6d7b493af4
SuperScript-PRC <2528622340@qq.com>
2024-04-04 05:34:53 +0800

    移除不必要的变量参数

--- tooldelta/frame.py
+++ tooldelta/frame.py
@@ -88,7 +88,7 @@ class Frame:
 
     @staticmethod
     def check_use_token(tok_name="", check_md=""):
-        res = sys_args.sys_args_to_dict(sys.argv)
+        res = sys_args.sys_args_to_dict()
         res = res.get(tok_name, 1)
         if (res == 1 and check_md) or res != check_md:
             Print.print_err("启动参数错误")
