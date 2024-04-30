17a1f0f42b1f62f6f6ecdd2a64db3fba4695e606
Happy2018new <chenbo79800@163.com>
2024-04-30 22:32:24 +0800

    remove `nbtlib` from this project
    * Remove "nbtlib=2.0.4" from this project
    * Remove function "place_command_bloc_with_command_block_data" from class "StandardFrame"
    * Minor adjust on class "CommandBlockNBTData" ("neo_conn.py" related)
    * Minor adjust on function "place_command_block_with_nbt_data" (class "FrameNeOmg" related)

--- tooldelta/neo_conn.py
+++ tooldelta/neo_conn.py
@@ -8,7 +8,6 @@ import enum
 from typing import Iterable, Tuple, Optional, Union, Any, Callable, List, Dict
 from threading import Thread
 from dataclasses import dataclass
-from nbtlib import tag
 from tooldelta.color_print import Print
 from tooldelta.packets import Packet_CommandOutput
 
@@ -354,21 +353,21 @@ class CommandBlockNBTData:
     """指代一个命令块的原始 NBT 数据 (ToolDelta 专有实现类)
     
     Args
-        Command (nbtlib.tag.String): 命令块所包含的命令，默认为 空字符串
-        CustomName (nbtlib.tag.String): 命令方块的悬浮文本，默认为 空字符串
-        TickDelay (nbtlib.tag.Int): 命令块使用的延迟，默认为 0
-        ExecuteOnFirstTick (tag.Byte): 是否在该命令块上使用第一个已选项(仅重复型命令块适用)，默认 启用
-        TrackOutput (tag.Byte): 是否在该命令块上启用命令执行输出，默认 启用
-        ConditionalMode (tag.Byte): 命令块是否是 `有条件的` ，默认为 无条件
-        Auto (tag.Byte): 命令块是否自动运行(无需红石控制)，默认为 自动运行(无需红石控制)
+        Command (str; TAG_String): 命令块所包含的命令，默认为 空字符串
+        CustomName (str; TAG_String): 命令方块的悬浮文本，默认为 空字符串
+        TickDelay (int; TAG_Int): 命令块使用的延迟，默认为 0
+        ExecuteOnFirstTick (bool; TAG_Byte): 是否在该命令块上使用第一个已选项(仅重复型命令块适用)，默认 启用
+        TrackOutput (bool; TAG_Byte): 是否在该命令块上启用命令执行输出，默认 启用
+        ConditionalMode (bool; TAG_Byte): 命令块是否是“有条件的”，默认为 无条件
+        Auto (bool; TAG_Byte): 命令块是否自动运行(无需红石控制)，默认为 自动运行(无需红石控制)
     """
-    Command: tag.String = tag.String("")
-    CustomName: tag.String = tag.String("")
-    TickDelay: tag.Int = tag.Int(0)
-    ExecuteOnFirstTick: tag.Byte = tag.Byte(1)
-    TrackOutput: tag.Byte = tag.Byte(1)
-    ConditionalMode: tag.Byte = tag.Byte(0)
-    Auto: tag.Byte = tag.Byte(1)
+    Command: str = ""
+    CustomName: str = ""
+    TickDelay: int = 0
+    ExecuteOnFirstTick: bool = True
+    TrackOutput: bool = True
+    ConditionalMode: bool = False
+    Auto: bool = True
 
 
 @dataclass
