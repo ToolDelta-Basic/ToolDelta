01eaf7b18851412dcef5abcd60a6e5140dbe421c
xingchentye <xingchenawa@qq.com>
2024-06-14 20:04:59 +0800

    添加部分事件常量表

--- tooldelta/packets.py
+++ tooldelta/packets.py
@@ -1,5 +1,8 @@
 "数据包类构建器"
 
+from dataclasses import dataclass
+
+@dataclass
 class PacketIDS:
     "数据包 ID 常量表"
     IDLogin = 1  # 登录
@@ -178,6 +181,104 @@ class PacketIDS:
     IDAgentAction = 181  # 代理行动
     IDChangeMobProperty = 182  # 改变生物属性
     IDPyRpc = 183  # Python 远程过程调用
+
+@dataclass
+class ActorEventType:
+    "演员事件常量表"
+    ActorEventJump = 1  # 角色跳跃
+    ActorEventHurt = 2  # 角色受伤
+    ActorEventDeath = 3  # 角色死亡
+    ActorEventStartAttacking = 4  # 开始攻击
+    ActorEventStopAttacking = 5  # 停止攻击
+    ActorEventTamingFailed = 6  # 驯服失败
+    ActorEventTamingSucceeded = 7  # 驯服成功
+    ActorEventShakeWetness = 8  # 摇动湿度
+    ActorEventUseItem = 9  # 使用物品
+    ActorEventEatGrass = 10  # 吃草
+    ActorEventFishhookBubble = 11  # 鱼钩泡泡
+    ActorEventFishhookFishPosition = 12  # 鱼钩鱼位置
+    ActorEventFishhookHookTime = 13  # 鱼钩勾住时间
+    ActorEventFishhookTease = 14  # 鱼钩逗弄
+    ActorEventSquidFleeing = 15  # 乌贼逃离
+    ActorEventZombieConverting = 16  # 僵尸转换
+    ActorEventPlayAmbient = 17  # 播放环境音乐
+    ActorEventSpawnAlive = 18  # 产生生物
+    ActorEventStartOfferFlower = 19  # 开始献花
+    ActorEventStopOfferFlower = 20  # 停止献花
+    ActorEventLoveHearts = 21  # 爱心
+    ActorEventVillagerAngry = 22  # 村民生气
+    ActorEventVillagerHappy = 23  # 村民开心
+    ActorEventWitchHatMagic = 24  # 女巫帽子魔法
+    ActorEventFireworksExplode = 25  # 烟花爆炸
+    ActorEventInLoveHearts = 26  # 爱心飞扬
+    ActorEventSilverfishMergeAnimation = 27  # 银鱼合并动画
+    ActorEventGuardianAttackSound = 28  # 守卫者攻击音效
+    ActorEventDrinkPotion = 29  # 喝药水
+    ActorEventThrowPotion = 30  # 扔药水
+    ActorEventCartWithPrimeTNT = 31  # 载有TNT的车
+    ActorEventPrimeCreeper = 32  # 充能的爬行者
+    ActorEventAirSupply = 33  # 空气供应
+    ActorEventAddPlayerLevels = 34  # 增加玩家等级
+    ActorEventGuardianMiningFatigue = 35  # 守卫者挖掘疲劳
+    ActorEventAgentSwingArm = 36  # 代理摆动手臂
+    ActorEventDragonStartDeathAnim = 37  # 龙开始死亡动画
+    ActorEventGroundDust = 38  # 地面灰尘
+    ActorEventShake = 39  # 摇动
+    ActorEventFeed = 57  # 喂养角色
+    ActorEventBabyEat = 60 # 宝宝吃东西
+    ActorEventInstantDeath = 61 # 瞬间死亡
+    ActorEventNotifyTrade = 62 # 通知交易
+    ActorEventLeashDestroyed = 63 # 绳索被销毁
+    ActorEventCaravanUpdated = 64 # 更新马车
+    ActorEventTalismanActivate = 65 # 法宝激活
+    ActorEventUpdateStructureFeature = 66 # 更新结构特征
+    ActorEventPlayerSpawnedMob = 67 # 玩家产生的生物
+    ActorEventPuke = 68 # 呕吐
+    ActorEventUpdateStackSize = 69 # 更新堆叠大小
+    ActorEventStartSwimming = 70 # 开始游泳
+    ActorEventBalloonPop = 71 # 气球爆破
+    ActorEventTreasureHunt = 72 # 寻宝
+    ActorEventSummonAgent = 73 # 召唤代理
+    ActorEventFinishedChargingCrossbow = 74 # 弩完成充能
+    ActorEventLandedOnGround = 75 # 降落在地面
+    ActorEventActorGrowUp = 76 # 角色成长
+    ActorEventVibrationDetected = 77 # 检测到振动
+    
+@dataclass
+class EventType:
+    "事件类型常量表"
+    EventTypeAchievementAwarded  = 0  # 成就奖励
+    EventTypeEntityInteract  = 1  # 实体交互
+    EventTypePortalBuilt  = 2  # 传送门建立
+    EventTypePortalUsed  = 3  # 传送门使用
+    EventTypeMobKilled  = 4  # 生物被杀
+    EventTypeCauldronUsed  = 5  # 釜子使用
+    EventTypePlayerDied  = 6  # 玩家死亡
+    EventTypeBossKilled  = 7  # Boss被杀
+    EventTypeAgentCommand  = 8  # 代理命令
+    EventTypeAgentCreated  = 9  # 代理创建（目前无用？）
+    EventTypePatternRemoved  = 10  # 模式移除
+    EventTypeSlashCommandExecuted  = 11  # 斜杠命令执行
+    EventTypeFishBucketed  = 12  # 鱼桶
+    EventTypeMobBorn  = 13  # 生物出生
+    EventTypePetDied  = 14  # 宠物死亡
+    EventTypeCauldronInteract  = 15  # 釜子交互
+    EventTypeComposterInteract  = 16  # 堆肥桶交互
+    EventTypeBellUsed  = 17  # 钟声
+    EventTypeEntityDefinitionTrigger  = 18  # 实体定义触发
+    EventTypeRaidUpdate  = 19  # 袭击更新
+    EventTypeMovementAnomaly  = 20  # 移动异常
+    EventTypeMovementCorrected  = 21  # 移动校正
+    EventTypeExtractHoney  = 22  # 提取蜂蜜
+    EventTypeTargetBlockHit  = 23  # 目标方块被击中
+    EventTypePiglinBarter  = 24  # 畏怯者交易
+    EventTypePlayerWaxedOrUnwaxedCopper  = 25  # 玩家给覆蜡/未覆蜡铜
+    EventTypeCodeBuilderRuntimeAction  = 26  # 代码构建器运行时动作
+    EventTypeCodeBuilderScoreboard  = 27  # 代码构建器计分板
+    EventTypeStriderRiddenInLavaInOverworld  = 28  # 熔岩中骑猪灵
+    EventTypeSneakCloseToSculkSensor  = 29  # 靠近鸾石感应器潜行
+
+
 
 class SubPacket_CmdOutputMsg:
     """命令输出消息子包构建"""
