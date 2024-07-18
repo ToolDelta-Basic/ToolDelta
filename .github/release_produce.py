9fd729ac024a6fa9899f12ec63ebb19ef82273ee
xingchentye <xingchenawa@qq.com>
2024-07-18 09:43:24 +0800

    💄使用新的ChangeLog样式

--- .github/release_produce.py
+++ .github/release_produce.py
@@ -1,29 +1,34 @@
 from git import Repo
 from datetime import datetime
 import pytz
+import os
 from packaging.version import parse
 
 
 def clone_repo(repo_url, repo_path, branch="main"):
-    Repo.clone_from(repo_url, to_path=repo_path, branch=branch)
+    if not os.path.exists(repo_path):
+        Repo.clone_from(repo_url, to_path=repo_path, branch=branch)
     return Repo(repo_path)
 
 
-def get_max_version_tag(repo):
+def get_max_version_tag(repo, n=1):
     tags = [tag for tag in repo.tags if tag.name != "binaries"]
-    return max(tags, key=lambda tag: parse(tag.name), default=None)
-
+    sorted_tags = sorted(tags, key=lambda tag: parse(tag.name), reverse=True)
+    return sorted_tags[n-1] if len(sorted_tags) >= n else None
 
 def get_local_time(utc_time, timezone="Asia/Shanghai"):
     local_tz = pytz.timezone(timezone)
     return utc_time.replace(tzinfo=pytz.utc).astimezone(local_tz)
 
 
-def generate_changelog(repo, max_version, tag_creation_date, version_file="version"):
-    tag_creation_datetime = datetime.strptime(tag_creation_date, "%Y-%m-%d %H:%M")
+def generate_changelog(repo, max_version, second_max_version, version_file="version"):
+    max_tag_creation_datetime = max_version.commit.committed_datetime
+    second_max_tag_creation_datetime = second_max_version.commit.committed_datetime
+
     new_commits_log = repo.git.log(
-        '--pretty={"commit":"%H","author":"%aN","summary":"%s","date":"%cd"}',
-        since=tag_creation_datetime,
+        '--pretty={"commit":"%H","author":"%cN","summary":"%s","date":"%cd"}',
+        since=second_max_tag_creation_datetime,
+        until=max_tag_creation_datetime,
         date="format:%Y-%m-%d %H:%M",
     )
 
@@ -34,15 +39,18 @@ def generate_changelog(repo, max_version, tag_creation_date, version_file="versi
 
     with open("changelog.md", "w") as CHANGELOG:
         CHANGELOG.write(
-            f"## ToolDelta Release v{ToolDeltaVersion} ({tag_creation_date})\n\n"
+            f"## ToolDelta Release v{ToolDeltaVersion} ({max_tag_creation_datetime.strftime('%Y-%m-%d %H:%M')})\n\n"
         )
         for commit in new_real_commits_list:
             commit_id = commit["commit"]
             author = commit["author"]
             summary = commit["summary"]
             date = commit["date"]
+            ColorCyan = "{Cyan}"
+            ColorOrange = "{Orange}"
+            ColorSteelBlue = "{SteelBlue}"
             CHANGELOG.write(
-                f"- [{commit_id[:7]}](https://github.com/ToolDelta/commit/{commit_id}): {summary} by {author} ({date})\n"
+                f"- [[`{commit_id[:7]}`](https://github.com/ToolDelta/commit/{commit_id})] $\color{ColorSteelBlue}{summary}$ By $\color{ColorCyan}{author}$ ($\color{ColorOrange}{date}$)\n"
             )
 
 
@@ -50,17 +58,29 @@ def main():
     repo_path = "/home/runner/work/Test"
     repo_url = "https://github.com/ToolDelta/ToolDelta.git"
 
+    # repo_path = "/home/xingchen/WorkSpace/ToolDelta/.github/test"
+    # repo_url = "https://tdload.tblstudio.cn/https://github.com/ToolDelta/ToolDelta.git"
+
     repo = clone_repo(repo_url, repo_path)
 
-    max_version = get_max_version_tag(repo)
-    if max_version:
+    max_version = get_max_version_tag(repo, 1)
+    second_max_version = get_max_version_tag(repo, 2)
+
+    if max_version and second_max_version:
         utc_time = max_version.commit.committed_datetime
         local_time = get_local_time(utc_time)
-        tag_creation_date = local_time.strftime("%Y-%m-%d %H:%M")
-        print(f"最大版本号: {max_version.name}")
-        print(f"Tag {max_version.name} 的创建日期是: {tag_creation_date}")
+        max_tag_creation_date = local_time.strftime("%Y-%m-%d %H:%M")
 
-        generate_changelog(repo, max_version, tag_creation_date)
+        utc_time = second_max_version.commit.committed_datetime
+        local_time = get_local_time(utc_time)
+        second_max_tag_creation_date = local_time.strftime("%Y-%m-%d %H:%M")
+
+        print(f"最大版本号: {max_version.name}")
+        print(f"第二大版本号: {second_max_version.name}")
+        print(f"Tag {max_version.name} 的创建日期是: {max_tag_creation_date}")
+        print(f"Tag {second_max_version.name} 的创建日期是: {second_max_tag_creation_date}")
+
+        generate_changelog(repo, max_version, second_max_version)
     else:
         print("No valid tags found.")
         exit()
