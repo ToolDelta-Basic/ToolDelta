6e2ce0d30a6b3ca5b965e5e7ee1fb3edc8a89c1a
SuperScript-PRC <2528622340@qq.com>
2025-02-01 10:34:35 +0800

    fix: cmd_is_executed

--- tooldelta/frame.py
+++ tooldelta/frame.py
@@ -597,7 +597,7 @@ class ToolDelta:
                                         break
                                     elif res == -1:
                                         return
-                    if cmd_is_executed and rsp:
+                    if not cmd_is_executed and rsp:
                         self.link_game_ctrl.say_to("@a", f"[§b控制台§r] §3{rsp}§r")
                 except Exception:
                     Print.print_err(f"控制台指令执行出现问题: {traceback.format_exc()}")
