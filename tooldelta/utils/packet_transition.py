433cf698b63690873a45e722009e3aea56f625d2
SuperScript-PRC <2528622340@qq.com>
2025-04-19 13:36:42 +0800

    fix: packet_transition: /me 消息不会处理特殊名

--- tooldelta/utils/packet_transition.py
+++ tooldelta/utils/packet_transition.py
@@ -34,15 +34,15 @@ def get_playername_and_msg_from_text_packet(
             return None, None, False
         case TextType.TextTypeChat | TextType.TextTypeWhisper:
             src_name = pkt["SourceName"]
-            playername = sender_name or to_plain_name(src_name)
+            playername = sender_name or src_name
             if src_name == "":
                 # /me 消息
                 msg_list = msg.split(" ")
                 if len(msg_list) >= 3:
-                    playername = playername or msg_list[1]
+                    playername = to_plain_name(playername or msg_list[1])
                     msg = " ".join(msg_list[2:])
                 else:
-                    fmts.print_war(f"无法获取发言中的玩家名与消息: {playername}: {msg}")
+                    fmts.print_war(f"[internal] 无法获取发言中的玩家名与消息: {playername}: {msg}")
                     return None, None, False
             return playername, msg, sender_name != ""
         case TextType.TextTypeAnnouncement:
