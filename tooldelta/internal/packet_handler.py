3fe32e4cd890406900d6b0f3cedbff4d0d11fe8b
SuperScript-PRC <2528622340@qq.com>
2025-05-15 10:22:45 +0800

    packet_handler: feat: wait_next_packet

--- tooldelta/internal/packet_handler.py
+++ tooldelta/internal/packet_handler.py
@@ -4,27 +4,35 @@ from collections.abc import Callable
 from ..mc_bytes_packet.base_bytes_packet import BaseBytesPacket
 from ..constants.packets import PacketIDS
 from ..constants.tooldelta_cli import INTERNAL_LISTEN_PACKETS
+from ..utils import create_result_cb
 
 if TYPE_CHECKING:
     from tooldelta.frame import ToolDelta
 
 
-
 DictPacketListener = Callable[[dict], bool]
 BytesPacketListener = Callable[[BaseBytesPacket], bool]
+DictPacketWaiter = Callable[[dict], None]
 
 
 class PacketHandler:
     def __init__(self, frame: "ToolDelta"):
         self.frame = frame
         self.listen_packets = INTERNAL_LISTEN_PACKETS.copy()
-
         self.dict_packet_listener_with_priority: dict[
             int, dict[int, list[DictPacketListener]]
         ] = {}
         self.bytes_packet_listener_with_priority: dict[
             int, dict[int, list[BytesPacketListener]]
         ] = {}
+        self.dict_packet_wait_callbacks: dict[int, list[DictPacketWaiter]] = {}
+
+    def wait_next_packet(self, pkID: PacketIDS, timeout: float = 30) -> dict | None:
+        getter, setter = create_result_cb(dict)
+        self.dict_packet_wait_callbacks.setdefault(pkID, []).append(setter)
+        res = getter(timeout)
+        self.dict_packet_wait_callbacks[pkID].remove(setter)
+        return res
 
     def add_dict_packet_listener(
         self,
@@ -57,6 +65,12 @@ class PacketHandler:
             plist.append(cb)
 
     def entrance_dict_packet(self, packetID: int, packet: dict):
+        self._handle_dict_packet(packetID, packet)
+
+    def entrance_bytes_packet(self, packetID: int, packet: BaseBytesPacket):
+        self._handle_bytes_packet(packetID, packet)
+
+    def _handle_dict_packet(self, packetID: int, packet: dict):
         pkt_cbs = self.dict_packet_listener_with_priority.get(packetID)
         if pkt_cbs:
             for priority in sorted(pkt_cbs.keys(), reverse=True):
@@ -64,10 +78,15 @@ class PacketHandler:
                     if cb(packet):
                         return
 
-    def entrance_bytes_packet(self, packetID: int, packet: BaseBytesPacket):
+    def _handle_bytes_packet(self, packetID: int, packet: BaseBytesPacket):
         pkt_cbs = self.bytes_packet_listener_with_priority.get(packetID)
         if pkt_cbs:
             for priority in sorted(pkt_cbs.keys(), reverse=True):
                 for cb in pkt_cbs[priority]:
                     if cb(packet):
                         return
+
+    def _handle_next_dict_packet(self, packetID: int, packet: dict):
+        cbs = self.dict_packet_wait_callbacks.get(packetID, [])
+        for cb in cbs:
+            cb(packet)
