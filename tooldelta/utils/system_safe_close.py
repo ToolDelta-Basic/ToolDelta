abda8875b30a0119aa7bf1b98e7efcee6ef99d71
SuperScript <2528622340@qq.com>
2025-06-15 17:42:42 +0800

    system_safe_close: minor fix

--- tooldelta/utils/system_safe_close.py
+++ tooldelta/utils/system_safe_close.py
@@ -10,7 +10,7 @@ def safe_close():
     """安全关闭: 保存JSON配置文件和关闭所有定时任务"""
     timer_events.timer_event_stop.set()
     timer_events.timer_events_clear()
-    fmts.print_inf("正在强制中断 ToolDeltThread..")
+    fmts.print_inf("正在强制中断 ToolDeltaThread..")
     tooldelta_thread.force_stop_normal_threads()
     fmts.print_inf("正在保存数据文件..")
     tempjson.save_all()
